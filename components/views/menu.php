e7521936f0cfe287c466c27ec48eee935a4251ae
Ринат <bagauv.rinat@yandex.ru>
2021-05-04 12:23:00 +0300

    test yii book project

--- components/views/menu.php
+++ components/views/menu.php
@@ -9,6 +9,7 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
@@ -17,9 +18,25 @@
                 </li>
                 <?php if(!empty($category)):?>
                     <?php foreach ($category as $k=>$v):?>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/web/book/category-book?id=<?=$v->category->id?>"><?=$v->category->name?> <span class="sr-only">(current)</span></a>
-                        </li>
+                        <?php if($v->category->parent_id == 0):?>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?=$v->category->name?></a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/web/book/category-book?id=<?=$v->category_id?>"><?=$v->category->name?></a>
+                                    <div class="dropdown-divider"></div>
+                                <?php foreach ($category as $key=>$value):?>
+                                    <?php if($value->category->parent_id == $v->category->id):?>
+
+                                            <a class="dropdown-item" href="/web/book/category-book?id=<?=$value->category_id?>"><?=$value->category->name?></a>
+                                            <div class="dropdown-divider"></div>
+
+
+                                    <?php endif;?>
+                                <?php endforeach;?>
+                                </div>
+                            </li>
+                        <?php endif;?>
                     <?php endforeach?>
                 <?php endif;?>
                 <li class="nav-item">
